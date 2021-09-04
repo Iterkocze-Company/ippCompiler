4a0c0075f9955246ce1977e20cae00b0276d312a
Michał <m12@onet.com.pl>
2021-09-05 00:25:41 +0200

    Initial commit

---
+++ ippCompiler/Compiler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ippCompiler
+{
+    public static class Compiler
+    {
+        private struct Compiler_Flags
+        {
+        }
+
+        //static string[] Tokens = {"echo"};
+
+        public static string[] ReadFileContents(string pathToFile)
+        {
+            return File.ReadAllText(pathToFile).Replace("\r\n", " ").Split(" ");
+        }
+
+        public static void Compile()
+        {
+            string[] contents = ReadFileContents(Program.CODE_FILE_PATH);
+            string[] GeneratedCode = new string[Program.FILE_LEN+10];
+            int index = 0;
+            foreach (string line in contents)
+            {
+                if (line == "echo")
+                {
+                    Console.WriteLine("echo wykryte!");
+                    GeneratedCode[index] = $"printf({contents[index + 1]} ";
+                    if (contents[index+2].EndsWith("\""))
+                        GeneratedCode[index] += $"{contents[index + 2]}";
+                    GeneratedCode[index] += ");";
+                        
+                    index++;
+                }
+            }
+
+            File.WriteAllText("genCode.c", "");
+            File.AppendAllText("genCode.c", "#include <stdio.h>\nint main() {");
+
+            for (int i = 0; i < GeneratedCode.Length; i++)
+                File.AppendAllText("genCode.c", "\n" + GeneratedCode[i]);
+
+            File.AppendAllText("genCode.c", "\nreturn 0;\n}");
+
+            Console.WriteLine("Kompiluję...");
+            Process.Start("gcc","genCode.c");
+        }
+    }
+}
---
+++ ippCompiler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ippCompiler
+{
+    public static class Program
+    {
+        public static string CODE_FILE_PATH = "";
+        public static int FILE_LEN = 0;
+        private static void SetFilePath(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wystąpił błąd podczas wczytywania pliku kodu! Upewnij się, że podełeś jego prawidłową lokalizację.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                CODE_FILE_PATH = path;
+                FILE_LEN = File.ReadLines(path).Count();
+            }
+                
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Witaj w kompilatorze dla języka i++");
+                Console.Write("Wprowadź ścieżkę do pliku języka i++ (relatywną): ");
+                SetFilePath(Console.ReadLine());
+                Compiler.Compile();
+            }
+        }
+    }
+}
